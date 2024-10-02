9b77f5595332957a8ed35ab18e20feb4784bfede
maguah <maguahofficial@gmail.com>
2024-10-02 21:33:05 +0300

    Finished Search functionality and utilized Weather Api

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,69 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import "./Weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloudy_icon from "../assets/cloudy.png";
+import drizzle_icon from "../assets/drizzle.png";
+import snowy_icon from "../assets/snowy.png";
+import humidity_icon from "../assets/humidity.png";
+import windy_icon from "../assets/wind.png";
+import rainy_icon from "../assets/rain.png";
 
 const Weather = () => {
+  const [weatherData, setWeatherData] = useState(false);
+  const inputRef = useRef();
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloudy_icon,
+    "02n": cloudy_icon,
+    "03d": cloudy_icon,
+    "03n": cloudy_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rainy_icon,
+    "09n": rainy_icon,
+    "10d": rainy_icon,
+    "10n": rainy_icon,
+    "13d": snowy_icon,
+    "13n": snowy_icon,
+  }
+
+  const search = async (city) => {
+    if (city === "") {
+        alert("Enter a city name");
+        return;
+    }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+      if(!response.ok) {
+        alert(data.message);
+        return;
+      }
+      console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
+    } catch (error) {
+        setWeatherData(false);
+        console.log("Error in fetching weather data");
+    }
+  };
+
+  useEffect(() => {
+    search("Nairobi");
+  }, []);
+
   return (
     <div className="weather">
       <div className="search-bar">
@@ -30,7 +93,7 @@ const Weather = () => {
       </>:<></>}
       
     </div>
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
